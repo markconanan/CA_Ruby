893b8f9906641fe3590abefe649e4d0e922354dd
Mark Conanan <mark.conanan@gmail.com>
2018-08-23 11:26:11 +1000

    initial commit

---
+++ RB_02.rb
@@ -0,0 +1,14 @@
+24*365
+60*24*365*10
+29*365*24*60
+3.0/2
+3/2.0
+4**2.0
+41.1%2
+puts "Hello World"
+name="Mark"
+puts "my name is #{name}"
+siblings=4
+puts "My number of siblings is #{siblings}"
+siblings+1
+puts "My number of siblings plus one is #{siblings+1}"
---
+++ ex1.rb
@@ -0,0 +1,2 @@
+puts "Hallow"
+puts "Hello World"
